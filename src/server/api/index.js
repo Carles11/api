86d0dfa83aa27686a785ada951d21e952974ebe3
Carles <carles@crix.design>
2020-11-15 02:05:14 +0100

    uncommented abluelemon routes

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -4,14 +4,14 @@ import { Router } from 'express'
 
 // import blog from './blog'
 import leo from './leo'
-// import abluelemon from './abluelemon'
-import terapias from './terapias'
+import abluelemon from './abluelemon'
+// import terapias from './terapias'
 
 const router = Router()
 
 // router.use('/blog', blog)
 router.use('/leo', leo)
-// router.use('/abluelemon', abluelemon)
+router.use('/abluelemon', abluelemon)
 // router.use('/terapias', terapias)
 
 export default router
