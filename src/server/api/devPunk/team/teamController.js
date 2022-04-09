2dee196ff79f611e8fabc28e95892efdfb017eef
Carles <carles@crix.design>
2022-04-09 15:28:17 -0300

    team response

--- src/server/api/devPunk/team/teamController.js
+++ src/server/api/devPunk/team/teamController.js
@@ -3,7 +3,28 @@ import Document from './teamModel'
 export const list = async (req, res, next) => {
   try {
     const members = await Document.find({})
-    res.status(201).json({ success: true, data: members })
+    console.log('MMMememememem', members)
+    res.status(201).json({
+      success: true,
+      data: [
+        {
+          firstName: 'Carles',
+          lastName: 'del Río',
+          email: 'contact@dev-punk.com',
+          position: 'CTO & Founder',
+          twitterUrl: '',
+          linkedInUrl: '',
+        },
+        {
+          firstName: 'Xavi',
+          lastName: 'del Río',
+          email: 'contact@dev-punk.com',
+          position: 'CEO & Founder',
+          twitterUrl: '',
+          linkedInUrl: '',
+        },
+      ],
+    })
   } catch (err) {
     res.status(500).json({ success: false, data: err })
   }
