8f0f065b7be22943ec16c8f17179f4d7943e6747
carlos <c.delriofrances@gmail.com>
2024-10-03 15:29:47 -0300

    extended cache expire life to 6 months

--- src/server/index.js
+++ src/server/index.js
@@ -13,9 +13,14 @@ middleware(app)
 
 // Middleware para controlar el caché
 app.use((req, res, next) => {
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate') // HTTP 1.1
-  res.setHeader('Pragma', 'no-cache') // HTTP 1.0
-  res.setHeader('Expires', '0') // Proxies
+  // Allow caching for up to 6 months
+  const maxAgeSeconds = 15778463 // 6 months in seconds
+  const maxAgeMilliseconds = maxAgeSeconds * 1000 // Convert to milliseconds
+  const expiresDate = new Date(Date.now() + maxAgeMilliseconds).toUTCString()
+
+  res.setHeader('Cache-Control', `public, max-age=${maxAgeSeconds}`)
+  res.setHeader('Pragma', 'cache') // HTTP 1.0
+  res.setHeader('Expires', expiresDate) // Expires after 6 months
   next()
 })
 
