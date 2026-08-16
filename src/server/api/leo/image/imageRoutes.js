46e8dab82146f6555be83546376fe8dd7cbacaa2
carles <c.delriofrances@gmail.com>
2026-08-16 17:17:39 -0400

    A-3: add auth to image POST / and PUT /:imageId
    Unauthenticated writes on the image endpoints were a defacement vector.
    Adds checkUser middleware (decodeToken + getFreshUser) to POST / and
    PUT /:imageId only. GET routes remain public for the photo gallery.

--- src/server/api/leo/image/imageRoutes.js
+++ src/server/api/leo/image/imageRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import * as ctrl from './imageController'
+import * as auth from '../../../auth'
 
+const checkUser = [auth.decodeToken(), auth.getFreshUser('leo')]
 const router = Router()
 
-router.route('/').get(ctrl.list).post(ctrl.create)
+router.route('/').get(ctrl.list).post(checkUser, ctrl.create)
 
-router.route('/:imageId').get(ctrl.read).put(ctrl.update)
+router.route('/:imageId').get(ctrl.read).put(checkUser, ctrl.update)
 
 router.route('/year/:year').get(ctrl.yearList)
 
